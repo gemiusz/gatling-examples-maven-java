2bc6886f69df046626f0356d8df733c46fb8346d
GeMi <gemi.github@drareg.pl>
2022-08-09 21:43:04 +0200

    Case0009SessionValuesSimulation Dynamically generating param values for an API and setting it using session -> https://community.gatling.io/t/dynamically-generating-param-values-for-an-api-and-setting-it-using-session/7041

---
+++ src/test/java/pl/gemiusz/Case0009SessionValuesSimulation.java
@@ -0,0 +1,43 @@
+package pl.gemiusz;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Session;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class Case0009SessionValuesSimulation extends Simulation {
+
+    HttpProtocolBuilder httpProtocol =
+            http
+                    .baseUrl("https://postman-echo.com");
+
+    ScenarioBuilder scn =
+            scenario("GeMi_SessionValuesSimulation")
+                    .exec(
+                            http("GeMi_SessionValuesSimulation_first")
+                                    .get("/status/414")
+                                    .check(status().is(414).saveAs("GeMi_Response_Code"))
+                    ).exec(session -> {
+                        System.out.println("GeMi_Response_Code: " + session.get("GeMi_Response_Code").toString());
+                        Session session1 = session.set("GeMi_Response_Code_1", session.get("GeMi_Response_Code").toString() + "_1");
+                        return session1;
+                    }).exec(session -> {
+                        System.out.println("GeMi_Response_Code_1: " + session.get("GeMi_Response_Code_1").toString());
+                        Session session2 = session.set("GeMi_Response_Code_2", session.get("GeMi_Response_Code_1").toString() + "_2");
+                        return session2;
+                    }).exec(session -> {
+                        System.out.println("GeMi_Response_Code_2: " + session.get("GeMi_Response_Code_2").toString());
+                        return session;
+                    }).exec(
+                            http("GeMi_SessionValuesSimulation_later")
+                                    .get("/get?foo=#{GeMi_Response_Code_2}")
+                                    .check(jmesPath("args.foo").is("414_1_2")));
+
+    {
+        setUp(scn.injectOpen(atOnceUsers(1)).protocols(httpProtocol));
+    }
+}
