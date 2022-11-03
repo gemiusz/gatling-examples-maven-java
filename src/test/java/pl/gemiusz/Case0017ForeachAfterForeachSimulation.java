defd4d149abb5b32215b24b9d54be3d906804bdd
GeMi <gemi.github@drareg.pl>
2022-11-03 16:49:35 +0100

    Case0017ForeachAfterForeachSimulation improvements after Code Review by @slandelle

--- src/test/java/pl/gemiusz/Case0017ForeachAfterForeachSimulation.java
+++ src/test/java/pl/gemiusz/Case0017ForeachAfterForeachSimulation.java
@@ -21,7 +21,16 @@ public class Case0017ForeachAfterForeachSimulation extends Simulation {
                     .exec(
                             http("GeMi_ForeachAfterForeachSimulation_langs_post")
                                     .post("/post")
-                                    .body(StringBody("{\"langs\":[\"pl\",\"fr\",\"en\"]}"))
+                                    .body(StringBody("""
+                                            {
+                                              "langs": [
+                                                "pl",
+                                                "fr",
+                                                "en"
+                                              ]
+                                            }
+                                            """)
+                                    )
                                     .asJson()
                                     .check(
                                             jmesPath("json.langs")
@@ -29,17 +38,39 @@ public class Case0017ForeachAfterForeachSimulation extends Simulation {
                                                     .saveAs("GeMi_langs")
                                     )
                     ).exec(session -> {
-                        System.out.println("GeMi_langs: " + session.get("GeMi_langs").toString());
+                        System.out.println("GeMi_langs: " + session.getString("GeMi_langs"));
                         return session;
                     }).foreach("#{GeMi_langs}", "GeMi_langs_el").on(
                             exec(session -> {
-                                System.out.println("GeMi_langs_el: " + session.get("GeMi_langs_el").toString());
+                                System.out.println("GeMi_langs_el: " + session.getString("GeMi_langs_el"));
                                 return session;
                             })
                     ).exec(
                             http("GeMi_ForeachAfterForeachSimulation_curses_post")
                                     .post("/post")
-                                    .body(StringBody("{\"curses\":[{\"pictureId\":1,\"language\":\"pl\"},{\"pictureId\":3,\"language\":\"en\"},{\"pictureId\":2,\"language\":\"fr\"},{\"pictureId\":5,\"language\":\"pl\"}]}"))
+                                    .body(StringBody("""
+                                            {
+                                              "curses": [
+                                                {
+                                                  "pictureId": 1,
+                                                  "language": "pl"
+                                                },
+                                                {
+                                                  "pictureId": 3,
+                                                  "language": "en"
+                                                },
+                                                {
+                                                  "pictureId": 2,
+                                                  "language": "fr"
+                                                },
+                                                {
+                                                  "pictureId": 5,
+                                                  "language": "pl"
+                                                }
+                                              ]
+                                            }
+                                            """)
+                                    )
                                     .asJson()
                                     .check(
                                             jmesPath("json.curses")
@@ -47,33 +78,33 @@ public class Case0017ForeachAfterForeachSimulation extends Simulation {
                                                     .saveAs("GeMi_curses")
                                     )
                     ).exec(session -> {
-                        System.out.println("GeMi_curses: " + session.get("GeMi_curses").toString());
+                        System.out.println("GeMi_curses: " + session.getString("GeMi_curses"));
                         return session;
                     }).foreach("#{GeMi_curses}", "GeMi_curses_el").on(
                             exec(session -> {
-                                System.out.println("GeMi_curses_el: " + session.get("GeMi_curses_el").toString());
+                                System.out.println("GeMi_curses_el: " + session.getString("GeMi_curses_el"));
                                 return session;
                             })
                     ).foreach("#{GeMi_langs}", "GeMi_langs_el").on(
                             exec(session -> {
                                 System.out.println("------------------------------------------------------------------------------");
-                                System.out.println("- GeMi_langs_el: " + session.get("GeMi_langs_el").toString());
+                                System.out.println("- GeMi_langs_el: " + session.getString("GeMi_langs_el"));
                                 return session;
                             }).foreach("#{GeMi_curses}", "GeMi_curses_el").on(
                                     exec(session -> {
-                                        System.out.println("-- GeMi_curses_el: " + session.get("GeMi_curses_el").toString());
-                                        Map<String, Object> gemiCursesElObj = session.get("GeMi_curses_el");
-                                        if (session.get("GeMi_langs_el").toString().equals(gemiCursesElObj.get("language").toString())) {
-                                            System.out.println("--- GeMi_langs_el EQ GeMi_curses_el_language -> " + session.get("GeMi_langs_el").toString() + " " + session.get("GeMi_curses_el").toString());
+                                        System.out.println("-- GeMi_curses_el: " + session.getString("GeMi_curses_el"));
+                                        Map<String, Object> gemiCursesElObj = session.getMap("GeMi_curses_el");
+                                        if (session.getString("GeMi_langs_el").equals(gemiCursesElObj.get("language").toString())) {
+                                            System.out.println("--- GeMi_langs_el EQ GeMi_curses_el_language -> " + session.getString("GeMi_langs_el") + " " + session.getString("GeMi_curses_el"));
                                             Session sessionTrue = session.set("GeMi_pictureId", gemiCursesElObj.get("pictureId")).set("GeMi_isThisLang", true);
                                             return sessionTrue;
                                         }
-                                        System.out.println("--- GeMi_langs_el NE GeMi_curses_el_language -> " + session.get("GeMi_langs_el").toString() + " " + session.get("GeMi_curses_el").toString());
+                                        System.out.println("--- GeMi_langs_el NE GeMi_curses_el_language -> " + session.getString("GeMi_langs_el") + " " + session.getString("GeMi_curses_el"));
                                         Session sessionFalse = session.set("GeMi_isThisLang", false);
                                         return sessionFalse;
                                     }).doIf("#{GeMi_isThisLang}").then(
                                             exec(session -> {
-                                                System.out.println("---- Do something using GeMi_pictureId: " + session.get("GeMi_pictureId").toString());
+                                                System.out.println("---- Do something using GeMi_pictureId: " + session.getString("GeMi_pictureId"));
                                                 return session;
                                             }).exec(
                                                     http("GeMi_ForeachAfterForeachSimulation_pictureId_#{GeMi_pictureId}_get")
