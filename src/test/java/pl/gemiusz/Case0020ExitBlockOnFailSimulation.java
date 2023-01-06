a240a6be70796bf0f6bc1b54ee5198163e31fede
GeMi <gemi.github@drareg.pl>
2023-01-06 21:39:23 +0100

    Case0020ExitBlockOnFailSimulation Stop the current Iteration/loop and start the next Iteration/loop when request failed -> https://community.gatling.io/t/stop-the-current-iteration-loop-and-start-the-next-iteration-loop-when-request-failed/7492

---
+++ src/test/java/pl/gemiusz/Case0020ExitBlockOnFailSimulation.java
@@ -0,0 +1,40 @@
+package pl.gemiusz;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import java.time.Duration;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class Case0020ExitBlockOnFailSimulation extends Simulation {
+
+    HttpProtocolBuilder httpProtocol =
+            http
+                    .baseUrl("https://postman-echo.com");
+
+    ScenarioBuilder scn =
+            scenario("GeMi_ExitBlockOnFailSimulation")
+                    .exitBlockOnFail(
+                            exec(
+                                    http("GeMi_ExitBlockOnFailSimulatio_get_200")
+                                            .get("/status/200")
+                                            .check(status().is(200))
+                            ).exec(
+                                    http("GeMi_ExitBlockOnFailSimulatio_get_200_or_exit")
+                                            .get("/status/20#{randomInt(0,2)}")
+                                            .check(status().is(200))
+                            ).exec(
+                                    http("GeMi_ExitBlockOnFailSimulatio_get_400")
+                                            .get("/status/400")
+                                            .check(status().is(400))
+                            )
+                    );
+
+    {
+        setUp(scn.injectOpen(constantUsersPerSec(1).during(Duration.ofSeconds(10))).protocols(httpProtocol));
+    }
+}
