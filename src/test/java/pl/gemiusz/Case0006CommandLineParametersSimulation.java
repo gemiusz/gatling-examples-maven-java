eb303bd1218050935ece8d0081f5b976f7d2ff51
GeMi <gemi.github@drareg.pl>
2023-05-26 12:11:08 +0200

    Case0006CommandLineParametersSimulation Update to cover Assertions case Assertion in parameter -> https://community.gatling.io/t/assertion-in-parameter/7970

--- src/test/java/pl/gemiusz/Case0006CommandLineParametersSimulation.java
+++ src/test/java/pl/gemiusz/Case0006CommandLineParametersSimulation.java
@@ -9,12 +9,13 @@ import static io.gatling.javaapi.http.HttpDsl.http;
 
 /**
  * HOW TO RUN:
- * mvnw gatling:test -Dgatling.simulationClass=pl.gemiusz.Case0006CommandLineParametersSimulation -Dfoo=10 -Dbar=GeMi
+ * mvnw gatling:test -Dgatling.simulationClass=pl.gemiusz.Case0006CommandLineParametersSimulation -Dfoo=10 -Dbar=GeMi -Dpercentile3responseTime=15
  */
 public class Case0006CommandLineParametersSimulation extends Simulation {
 
     int foo = Integer.getInteger("foo", 1);
     String bar = System.getProperty("bar");
+    int percentile3responseTime = Integer.getInteger("percentile3responseTime", 2000);
 
     HttpProtocolBuilder httpProtocol =
             http
@@ -23,13 +24,33 @@ public class Case0006CommandLineParametersSimulation extends Simulation {
     ScenarioBuilder scn =
             scenario("GeMi_CommandLineParametersSimulation")
                     .exec(
-                            http("GeMi_CommandLineParametersSimulation_get")
+                            http("GeMi_CommandLineParametersSimulation_get_1")
+                                    .get("/get?foo=" + foo + "&bar=" + bar)
+                                    .check(jmesPath("args.foo").is(String.valueOf(foo)))
+                                    .check(jmesPath("args.bar").is(String.valueOf(bar)))
+                    ).exec(
+                            http("GeMi_CommandLineParametersSimulation_get_2")
                                     .get("/get?foo=" + foo + "&bar=" + bar)
                                     .check(jmesPath("args.foo").is(String.valueOf(foo)))
                                     .check(jmesPath("args.bar").is(String.valueOf(bar)))
                     );
 
     {
-        setUp(scn.injectOpen(atOnceUsers(1)).protocols(httpProtocol));
+        setUp(scn.injectOpen(atOnceUsers(1)).protocols(httpProtocol)).assertions(
+                        global()
+                                .successfulRequests()
+                                .percent()
+                                .is(100.0)
+                )
+                .assertions(
+                        details("GeMi_CommandLineParametersSimulation_get_1")
+                                .responseTime()
+                                .percentile3()
+                                .lt(percentile3responseTime),
+                        details("GeMi_CommandLineParametersSimulation_get_2")
+                                .responseTime()
+                                .percentile3()
+                                .lt(percentile3responseTime));
+
     }
 }
