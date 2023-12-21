e8b855ee51b003c3ae0052ee206235547d94d0b5
GeMi <gemi.github@drareg.pl>
2023-12-21 11:44:48 +0100

    Update ComputerDatabaseSimulation to same version as is in Gatling 3.10.3 Bundle. Update BasicSimulation as is in Documentation (20231221)

--- src/test/java/computerdatabase/BasicSimulation.java
+++ src/test/java/computerdatabase/BasicSimulation.java
@@ -1,75 +1,51 @@
-/*
- * Copyright 2011-2022 GatlingCorp (https://gatling.io)
+package computerdatabase; // 1
+
+// 2
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
+
+/***
+ * What does it mean?
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 1. The optional package.
+ * 2. The required imports.
+ * 3. The class declaration. Note that it extends Simulation.
+ * 4. The common configuration to all HTTP requests.
+ * 5. The baseUrl that will be prepended to all relative urls.
+ * 6. Common HTTP headers that will be sent with all the requests.
+ * 7. The scenario definition.
+ * 8. An HTTP request, named request_1. This name will be displayed in the final reports.
+ * 9. The url this request targets with the GET method.
+ * 10. Some pause/think time.
+ * Duration units default to seconds, e.g. pause(5) is equivalent to java.time.Duration.ofSeconds(5) in Java or pause(5.seconds) in Scala.
+ * 11. Where one sets up the scenarios that will be launched in this Simulation.
+ * 12. Declaring that we will inject one single user into the scenario named scn.
+ * 13. Attaching the HTTP configuration declared above.
  */
+public class BasicSimulation extends Simulation { // 3
 
-package computerdatabase;
-
-import static io.gatling.javaapi.core.CoreDsl.*;
-import static io.gatling.javaapi.http.HttpDsl.*;
-
-import io.gatling.javaapi.core.*;
-import io.gatling.javaapi.http.*;
-import java.time.Duration;
-
-public class BasicSimulation extends Simulation {
-
-  HttpProtocolBuilder httpProtocol =
-      http
-          // Here is the root for all relative URLs
-          .baseUrl("http://computer-database.gatling.io")
-          // Here are the common headers
-          .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+  HttpProtocolBuilder httpProtocol = http // 4
+          .baseUrl("http://computer-database.gatling.io") // 5
+          .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
           .doNotTrackHeader("1")
           .acceptLanguageHeader("en-US,en;q=0.5")
           .acceptEncodingHeader("gzip, deflate")
-          .userAgentHeader(
-              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0"
-          );
+          .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0");
 
-  // A scenario is a chain of requests and pauses
-  ScenarioBuilder scn =
-      scenario("Scenario Name")
-          .exec(http("request_1").get("/"))
-          // Note that Gatling has recorded real time pauses
-          .pause(7)
-          .exec(http("request_2").get("/computers?f=macbook"))
-          .pause(2)
-          .exec(http("request_3").get("/computers/6"))
-          .pause(3)
-          .exec(http("request_4").get("/"))
-          .pause(2)
-          .exec(http("request_5").get("/computers?p=1"))
-          .pause(Duration.ofMillis(670))
-          .exec(http("request_6").get("/computers?p=2"))
-          .pause(Duration.ofMillis(629))
-          .exec(http("request_7").get("/computers?p=3"))
-          .pause(Duration.ofMillis(734))
-          .exec(http("request_8").get("/computers?p=4"))
-          .pause(5)
-          .exec(http("request_9").get("/computers/new"))
-          .pause(1)
-          .exec(
-              http("request_10")
-                  // Here's an example of a POST request
-                  .post("/computers")
-                  .formParam("name", "Beautiful Computer")
-                  .formParam("introduced", "2012-05-30")
-                  .formParam("discontinued", "")
-                  .formParam("company", "37"));
+  ScenarioBuilder scn = scenario("BasicSimulation") // 7
+          .exec(http("request_1") // 8
+                  .get("/")) // 9
+          .pause(5); // 10
 
   {
-    setUp(scn.injectOpen(atOnceUsers(1)).protocols(httpProtocol));
+    setUp( // 11
+            scn.injectOpen(atOnceUsers(1)) // 12
+    ).protocols(httpProtocol); // 13
   }
 }
--- src/test/java/computerdatabase/ComputerDatabaseSimulation.java
+++ src/test/java/computerdatabase/ComputerDatabaseSimulation.java
@@ -1,28 +1,16 @@
-/*
- * Copyright 2011-2022 GatlingCorp (https://gatling.io)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package computerdatabase;
 
-import static io.gatling.javaapi.core.CoreDsl.*;
-import static io.gatling.javaapi.http.HttpDsl.*;
-
-import io.gatling.javaapi.core.*;
-import io.gatling.javaapi.http.*;
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.FeederBuilder;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import java.util.concurrent.ThreadLocalRandom;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
 
 /**
  * This sample is based on our official tutorials:
@@ -39,38 +27,30 @@ public class ComputerDatabaseSimulation extends Simulation {
                     .acceptLanguageHeader("en-US,en;q=0.5")
                     .acceptEncodingHeader("gzip, deflate")
                     .userAgentHeader(
-                            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0"
-                    );
+                            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0");
 
     FeederBuilder<String> feeder = csv("feeders/search.csv").random();
 
     ChainBuilder search =
-            exec(http("Home").get("/"))
-                    .pause(1)
-                    .feed(feeder)
-                    .exec(
-                            http("Search")
-                                    .get("/computers?f=#{searchCriterion}")
-                                    .check(
-                                            css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")
-                                    )
-                    )
-                    .pause(1)
-                    .exec(
-                            http("Select")
-                                    .get("#{computerUrl}")
-                                    .check(status().is(200))
-                    )
-                    .pause(1);
+            // Execute actions sequentially
+            exec(
+                    http("Home").get("/"),
+                    pause(1),
+                    feed(feeder),
+                    http("Search")
+                            .get("/computers?f=#{searchCriterion}")
+                            .check(css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")),
+                    pause(1),
+                    http("Select").get("#{computerUrl}").check(status().is(200)),
+                    pause(1)
+            );
 
     // Repeat is a loop resolved at RUNTIME
     ChainBuilder browse =
             // Note how we force the counter name, so we can reuse it
             repeat(4, "i").on(
-                    exec(
-                            http("Page #{i}")
-                                    .get("/computers?p=#{i}")
-                    ).pause(1)
+                    http("Page #{i}").get("/computers?p=#{i}"),
+                    pause(1)
             );
 
     // Note we should be using a feeder here
@@ -80,25 +60,21 @@ public class ComputerDatabaseSimulation extends Simulation {
             // Let's try at max 2 times
             tryMax(2)
                     .on(
-                            exec(
-                                    http("Form")
-                                            .get("/computers/new")
-                            )
-                                    .pause(1)
-                                    .exec(
-                                            http("Post")
-                                                    .post("/computers")
-                                                    .formParam("name", "Beautiful Computer")
-                                                    .formParam("introduced", "2012-05-30")
-                                                    .formParam("discontinued", "")
-                                                    .formParam("company", "37")
-                                                    .check(
-                                                            status().is(
-                                                                    // We do a check on a condition that's been customized with
-                                                                    // a lambda. It will be evaluated every time a user executes
-                                                                    // the request.
-                                                                    session -> 200 + ThreadLocalRandom.current().nextInt(2)
-                                                            )
+                            http("Form").get("/computers/new"),
+                            pause(1),
+                            http("Post")
+                                    .post("/computers")
+                                    .formParam("name", "Beautiful Computer")
+                                    .formParam("introduced", "2012-05-30")
+                                    .formParam("discontinued", "")
+                                    .formParam("company", "37")
+                                    .check(
+                                            status()
+                                                    .is(
+                                                            // We do a check on a condition that's been customized with
+                                                            // a lambda. It will be evaluated every time a user executes
+                                                            // the request.
+                                                            session -> 200 + ThreadLocalRandom.current().nextInt(2)
                                                     )
                                     )
                     )
